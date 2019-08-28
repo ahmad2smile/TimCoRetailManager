3cf9337cebf89d551a48b765871ff735da6a9341
Ahmad <ahmad2smile@protonmail.com>
2019-08-28 18:00:32 +0700

    Swager Setup with AuthorizationOperationFilter and AuthTokenDocumentFilter

---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,26 @@
+﻿using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace TRMDataManager
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "Access Token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
